b21f4cb36610ef58188320faa545a4737f20037b
Vo Thi Bao Tran <tranb9485@gmail.com>
2026-07-26 04:46:10 +0000

    them ho tro trong lib

--- mod/invenioresource/lib.php
+++ mod/invenioresource/lib.php
@@ -10,6 +10,12 @@ function invenioresource_supports($feature)
         case FEATURE_MOD_INTRO:
             return true;
 
+        case FEATURE_SHOW_DESCRIPTION:
+            return true;
+
+        case FEATURE_COMPLETION_TRACKS_VIEWS:
+            return true;
+
         case FEATURE_BACKUP_MOODLE2:
             return true;
 
